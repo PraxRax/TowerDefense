43cded3d4e660a648273f9cc59abdab3ed781d25
PraxRax <62686404+PraxRax@users.noreply.github.com>
2020-06-08 16:40:24 +0200

    Revert "added the game"
    This reverts commit 03c4a8652ac72dfceb0bbe0332c7d0b928f8e13c.

--- Tower Defense/Assets/Scripts/ScriptableObjects_Scripts/ScriptableObjects_Turrets/ScriptableTurretHead.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "new_TurretHead_ScriptableObject", menuName = "ScriptableObjects/ScriptableTurretHead")]
-public class ScriptableTurretHead : ScriptableObject
-{
-    public string TagForEnemy;
-}
